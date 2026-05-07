a6370079cbf221e9956e8a62005645d7d680b05d
Kunpeng <76896982+Kunpeng-chen@users.noreply.github.com>
2026-05-07 18:18:14 +0800

    Add router API declarations

--- include/lrp.h
+++ include/lrp.h
@@ -35,6 +35,18 @@ bool lrp_dedup_check_and_remember(
     uint32_t now_ms
 );
 
+void lrp_router_init(
+    lrp_router_t *router,
+    const lrp_router_config_t *config
+);
+
+lrp_decision_t lrp_router_on_frame(
+    lrp_router_t *router,
+    const lrp_frame_t *in,
+    lrp_frame_t *out,
+    uint32_t now_ms
+);
+
 #ifdef __cplusplus
 }
 #endif
