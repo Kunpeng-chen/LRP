762086d232f0e74f39f9dd39b750a7656ab33ffa
Kunpeng <76896982+Kunpeng-chen@users.noreply.github.com>
2026-05-07 16:39:32 +0800

    Add LRP public API header

---
+++ include/lrp.h
@@ -0,0 +1,32 @@
+#ifndef LRP_H
+#define LRP_H
+
+#include "lrp_type.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+lrp_status_t lrp_frame_encode(
+    const lrp_frame_t *frame,
+    uint8_t *out,
+    uint16_t out_size,
+    uint16_t *out_len
+);
+
+lrp_status_t lrp_frame_decode(
+    const uint8_t *data,
+    uint16_t len,
+    lrp_frame_t *out
+);
+
+uint16_t lrp_crc16_ccitt(
+    const uint8_t *data,
+    uint16_t len
+);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* LRP_H */
