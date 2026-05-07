a0e36a5d74e5f5a84bf8de44b1510097b86110be
Kunpeng <76896982+Kunpeng-chen@users.noreply.github.com>
2026-05-07 19:16:22 +0800

    Refactor lrp.h into protocol stack umbrella header

--- include/lrp.h
+++ include/lrp.h
@@ -2,53 +2,8 @@
 #define LRP_H
 
 #include "lrp_type.h"
-
-#ifdef __cplusplus
-extern "C" {
-#endif
-
-lrp_status_t lrp_frame_encode(
-    const lrp_frame_t *frame,
-    uint8_t *out,
-    uint16_t out_size,
-    uint16_t *out_len
-);
-
-lrp_status_t lrp_frame_decode(
-    const uint8_t *data,
-    uint16_t len,
-    lrp_frame_t *out
-);
-
-uint16_t lrp_crc16_ccitt(
-    const uint8_t *data,
-    uint16_t len
-);
-
-void lrp_dedup_init(
-    lrp_dedup_cache_t *cache
-);
-
-bool lrp_dedup_check_and_remember(
-    lrp_dedup_cache_t *cache,
-    const lrp_frame_t *frame,
-    uint32_t now_ms
-);
-
-void lrp_router_init(
-    lrp_router_t *router,
-    const lrp_router_config_t *config
-);
-
-lrp_decision_t lrp_router_on_frame(
-    lrp_router_t *router,
-    const lrp_frame_t *in,
-    lrp_frame_t *out,
-    uint32_t now_ms
-);
-
-#ifdef __cplusplus
-}
-#endif
+#include "lrp_frame.h"
+#include "lrp_dedup.h"
+#include "lrp_router.h"
 
 #endif /* LRP_H */
